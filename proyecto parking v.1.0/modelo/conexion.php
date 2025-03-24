ea9b540528e0a570c66bcae13f7871d67d0fcff4
Emanuel Buritica <emanuelburitica269@gmail.com>
2025-03-23 19:54:53 -0500

    Add files via upload
    Version 1.0 de Refugio Rodante, estructura

---
+++ proyecto parking v.1.0/modelo/conexion.php
@@ -0,0 +1,20 @@
+<?php
+class conexion {
+  static public function conectar() {
+    try {
+      $link = new PDO(
+        "mysql:host=localhost;port=3308;dbname=rodante",
+        "root",
+        "",
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+      );
+      
+      $link->exec("set names utf8");
+      return $link;
+      
+    } catch(PDOException $e) {
+      echo "Error de conexión: " . $e->getMessage();
+      return null;
+    }
+  }
+}
